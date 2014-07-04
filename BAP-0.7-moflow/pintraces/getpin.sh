1dc0f1f9a807dbadcd227ea2c3c77e37004127f0
richinseattle <richinseattle@gmail.com>
2014-07-03 17:44:48 -0700

    updated getpin.sh to use https (h/t @marshray)

--- BAP-0.7-moflow/pintraces/getpin.sh
+++ BAP-0.7-moflow/pintraces/getpin.sh
@@ -11,7 +11,7 @@ cd $DIR
 
 # check if pin dir exists first
 
-wget 'http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.12-58423-gcc.4.4.7-linux.tar.gz' -U "Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0" -O - | tar -xvz -C ..
+wget 'https://software.intel.com/sites/landingpage/pintool/downloads/pin-2.12-58423-gcc.4.4.7-linux.tar.gz' -U "Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0" -O - | tar -xvz -C ..
 rm -rf ../pin
 mv ../pin-* ../pin
 #make
